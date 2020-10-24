6c048834beb83a2db0284f64c0a338508bca5a4d
sschiwek <steffen.schiwek@gmx.de>
2020-10-25 00:20:19 +0200

    creating quasicron for image creation

---
+++ myquasicronjob.R
@@ -0,0 +1,26 @@
+library(httr)
+print('creating image')
+system(command = 'Rscript createImage.R')
+print(paste0('image created: ', Sys.time()))
+mywebcrawl <-
+  GET(
+    'https://opendata.arcgis.com/datasets/dd4580c810204019a7b8eb3e0b329dd6_0.geojson'
+  )
+lastModified <- mywebcrawl$headers$`last-modified`
+print(paste0('Content Last Modified: ',lastModified))
+while(TRUE==TRUE){
+  mywebcrawl <-
+    GET(
+      'https://opendata.arcgis.com/datasets/dd4580c810204019a7b8eb3e0b329dd6_0.geojson'
+    )
+  lastModified1 <- mywebcrawl$headers$`last-modified`
+  print(paste0('Content Last Modified: ',lastModified1))
+  print(paste0('checking at: ',Sys.time()))
+  if(lastModified1!=lastModified){
+    system(command = 'Rscript createImage.R')
+    lastModified <- lastModified1
+  }
+  
+  #wait for one hour, then check again.
+  Sys.sleep(3600)
+}
