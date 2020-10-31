d133241a4c137bdf5bfb629f09a735ace0b3b94b
sschiwek <steffen.schiwek@gmx.de>
2020-10-31 22:42:03 +0100

    start_services.sh bugs

--- start_services.sh
+++ start_services.sh
@@ -1,8 +1,8 @@
-echo 'installing packaages'
+echo 'installing packages'
 nohup Rscript installPackages.R > loginstallPackages.log
 echo 'finished installing packages (or all needed packages were already installed)'
 echo 'start first import of data and image creation'
-nohup Rscript createimage.R > logcreateimage.log
+nohup Rscript createImage.R > logcreateimage.log
 echo 'finished fist import, starting quasicronjob'
 nohup Rscript myquasicronjob.R > logmyquasicronjob.log 2>&1 &
 echo 'starting app'
