20ee9056d1fc89b77fae2e232d6f432cb6181335
sschiwek <steffen.schiwek@gmx.de>
2020-10-31 22:31:28 +0100

    added installPackages to start_services

--- start_services.sh
+++ start_services.sh
@@ -1,3 +1,6 @@
+echo 'installing packaages'
+nohup Rscript installPackages.R > installPackages.log
+echo 'finished installing packages (or all needed packages were already installed'
 echo 'start first import of data and image creation'
 nohup Rscript test.R > createimage.log
 echo 'finished fist import, starting quasicronjob'
