d3175cfaafb2578f1e45b8fd1debd07e67fc4587
sschiwek <steffen.schiwek@gmx.de>
2020-10-31 22:36:40 +0100

    finetuning start_services

--- start_services.sh
+++ start_services.sh
@@ -1,10 +1,10 @@
 echo 'installing packaages'
-nohup Rscript installPackages.R > installPackages.log
-echo 'finished installing packages (or all needed packages were already installed'
+nohup Rscript installPackages.R > loginstallPackages.log
+echo 'finished installing packages (or all needed packages were already installed)'
 echo 'start first import of data and image creation'
-nohup Rscript test.R > createimage.log
+nohup Rscript createimage.R > logcreateimage.log
 echo 'finished fist import, starting quasicronjob'
-nohup Rscript myquasicronjob.R > myquasicronjob.log 2>&1 &
+nohup Rscript myquasicronjob.R > logmyquasicronjob.log 2>&1 &
 echo 'starting app'
-nohup Rscript app.R > app.log 2>&1 &
+nohup Rscript app.R > logapp.log 2>&1 &
 echo 'all processes are running'
