13bfc48674d84cc29f90796cdd4e00496f5bcb23
sschiwek <steffen.schiwek@gmx.de>
2020-10-31 22:14:33 +0100

    playing with test2

--- test2.sh
+++ test2.sh
@@ -1,2 +1,7 @@
-nohup Rscript test.R  > test.log 2>&1 &
-nohup Rscript test.R  > test1.log 2>&1 &
+Rscript test.R > test2.log 2>&1 &
+echo 'finished first'
+nohup Rscript test.R
+echo 'finished second'
+nohup Rscript test.R > test.log 2>&1 &
+nohup Rscript test.R > test1.log 2>&1 &
+echo 'finished all'
